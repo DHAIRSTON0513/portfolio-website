b45af90329f4abcba6416f3935422e6cea5e765b
Danielle Hairston <74224710+DHAIRSTON0513@users.noreply.github.com>
2020-12-06 20:28:45 -0700

    Add jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
 
-console.log("Hi there! My name is Danielle and this is my portolfio! Welcome!")
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000, 'linear');
+});
